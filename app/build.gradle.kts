dfa3f0188cdba9c97cf1387db0ac4b947cf165d4
elina <117643112+thisis-elina@users.noreply.github.com>
2023-12-17 22:25:14 +0100

    placeholder images added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.cc221001.weather_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.cc221001.weather_app"
