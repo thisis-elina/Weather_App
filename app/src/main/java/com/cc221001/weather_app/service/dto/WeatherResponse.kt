ce598c49e48f52631ee09cedd9bc73bce6b6b12e
elina <117643112+thisis-elina@users.noreply.github.com>
2024-01-13 22:38:27 +0100

    comments

--- app/src/main/java/com/cc221001/weather_app/service/dto/WeatherResponse.kt
+++ app/src/main/java/com/cc221001/weather_app/service/dto/WeatherResponse.kt
@@ -3,6 +3,23 @@ package com.cc221001.weather_app.service.dto
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Data class representing the response received from the OpenWeatherMap API for weather information.
+ *
+ * @property base The internal parameter indicating the base data source of the response.
+ * @property clouds Information about cloudiness.
+ * @property cod Internal parameter indicating the status of the response.
+ * @property coord Geographic coordinates of the location.
+ * @property dt Time of data calculation, unix, UTC.
+ * @property id City ID.
+ * @property main Main weather parameters such as temperature, pressure, and humidity.
+ * @property name City name.
+ * @property sys Additional system information.
+ * @property timezone Timezone of the city, in seconds.
+ * @property visibility Visibility in meters.
+ * @property weather List of weather conditions in the response.
+ * @property wind Wind information.
+ */
 data class WeatherResponse(
     @SerializedName("base")
     val base: String,
