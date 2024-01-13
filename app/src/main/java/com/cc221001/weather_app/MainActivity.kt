ad06491abf305cb118285d0a72167cc711b2b409
elina <117643112+thisis-elina@users.noreply.github.com>
2024-01-14 00:45:38 +0100

    display current temp, weather status and city

--- app/src/main/java/com/cc221001/weather_app/MainActivity.kt
+++ app/src/main/java/com/cc221001/weather_app/MainActivity.kt
@@ -14,17 +14,21 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import com.cc221001.weather_app.service.dto.WeatherResponse
@@ -56,13 +60,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Weather_AppTheme {
+                val weather by viewModel.weather.collectAsState(null)
                 Column(Modifier.fillMaxSize()) {
+                    Box {
                     Image(
                         painter = painterResource(id = R.drawable.background_sunny),
                         contentDescription = "Background",
                         modifier = Modifier.fillMaxWidth(),
                         contentScale = ContentScale.FillWidth
                     )
+                        Column(
+                            Modifier
+                                .padding(top = 48.dp)
+                                .align(Alignment.TopCenter),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(text = weather?.main?.temp.toString(), fontSize = 48.sp, color = Color.White)
+                            Text(text = weather?.weather?.first()?.main.toString(), fontSize = 28.sp, color = Color.White)
+                            Text(text = weather?.name.toString(), fontSize = 18.sp, color = Color.White)
+                        }
+                    }
                     Box(
                         modifier = Modifier
                             .background(Color(0xFF010528))
